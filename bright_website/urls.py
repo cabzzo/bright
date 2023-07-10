974b4fd0018fe187d9a215bca88f6a81c1217d0d
cabzzo <cabzzo@gmail.com>
2023-07-10 17:35:54 -0400

    Update URL configurations for 'letsbright' app

--- bright_website/urls.py
+++ bright_website/urls.py
@@ -2,12 +2,12 @@ from django.contrib.auth import views as auth_views
 from django.contrib import admin
 from django.urls import path
 from django.urls import include
-from letsbright.views import register
+from letsbright.views import about, register
 
 urlpatterns = [
+    path('', include('letsbright.urls')),
     path('register/', register, name='register'),
     path('login/', auth_views.LoginView.as_view(), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('admin/', admin.site.urls),
-    path('', include('letsbright.urls')), 
-]
+    ]
